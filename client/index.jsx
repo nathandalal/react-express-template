f6e160bdf97b486682978f6a39b71c6706fd9529
Nathan Dalal <nathandalal@Nathans-MacBook-Pro.local>
2017-08-03 13:19:36 -0400

    added linting and updated bulma

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom'
 
 import Home from './components/home.jsx'
 import About from './components/about.jsx'
@@ -12,18 +12,21 @@ class Index extends React.Component {
 
   render() {
     let routes = [
-      { path: "/",           name: "Home", 			  btnType: "primary",   component: Home },
+      { path: "/",           name: "Home",        btnType: "primary",   component: Home },
       { path: "/about",      name: "About",       btnType: "info",      component: About }
     ]
 
     return (
       <Router>
-      	<div style={{padding: "5%"}}>
-          {routes.map(route => <Route exact path={route.path} component={route.component} key={route.path} />)}
+        <div style={{padding: "5%"}}>
+        <Switch>
+            {routes.map(route => <Route exact path={route.path} component={route.component} key={route.path} />)}
+            <Redirect to ="/"/>
+          </Switch>
           <div className="block">
             {routes.map(route => <Link to={route.path} className={`button is-${route.btnType}`} key={route.path}>{route.name}</Link>)}
           </div>
-      	</div>
+        </div>
       </Router>
     )
   }
